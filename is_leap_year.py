dcdcb6e72022be332ae5efa8c6dacf5483011fa4
Durmuş PALA <71263028+durmus-pala@users.noreply.github.com>
2021-02-08 13:11:21 +0300

    modified my code.

--- is_leap_year.py
+++ is_leap_year.py
@@ -1,9 +1,14 @@
-year = input("Please Enter year to check: ")
-if (int(year) % 4 == 0) and (int(year) % 100 != 0):
-    print(year, "is a leap year.")
-elif int(year) % 400 == 0:
-    print(year, "is a leap year.")
-else:
-    print(year, "is not a leap year.")
-    
-            
+while True:
+  year = input("Please enter a 4 digit year: ")
+  if year.isdigit() == True:
+    if (int(year) % 4 == 0) and (int(year) % 100 != 0):
+      print(year, "is a leap year")
+      break
+    elif int(year) % 400 == 0:
+      print(year, "is a leap year")
+      break
+    else:
+      print(year, "is not a leap year.")
+      break
+  else:
+    print("Please Enter a valid year.")
